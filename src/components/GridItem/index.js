eb187a800e240ec4a1eafeb555c0e9b45095baa9
niltonc <niltoncesarjr1@gmail.com>
2022-05-22 13:18:12 -0300

    feat(grid item): component

---
+++ src/components/GridItem/index.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from "react";
+import { FlatList, Image} from "react-native";
+
+import Products from 'src/services/products';
+
+import * as GridItem from './styles';
+
+import ItemImage from 'src/assets/grid_item.png';
+import AddItemImage from 'src/assets/add_item';
+
+export default function GridItemComponent(){
+
+  const newProducts = new Products();
+  const [product, setProduct] = useState([]);
+
+  useEffect(() => {
+      newProducts.products().then((response) => setProduct(response.data)).catch(erro => console.log(erro));
+    }, []);
+
+  const renderItem = ({ item }) => {
+      return (
+        <GridItem.Wrapper>
+        <GridItem.ImageContainer>
+        <Image source={ItemImage}/>
+        <GridItem.Button>
+        <AddItemImage/>
+        </GridItem.Button>
+        </GridItem.ImageContainer>
+
+        <GridItem.Category>
+        {item?.category}
+        </GridItem.Category>
+
+        <GridItem.Title>
+        {item?.title}
+        </GridItem.Title>
+
+        <GridItem.Price>
+        ${item?.price}
+        </GridItem.Price>
+
+        </GridItem.Wrapper>
+    );
+  };
+  
+  const columns = 2; 
+  return (
+      <FlatList
+        data={product}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+        showsHorizontalScrollIndicator={false}
+        numColumns= {columns}
+        nestedScrollEnabled={true}
+      />
+  );
+};
---
+++ src/components/GridItem/styles.js
@@ -0,0 +1,43 @@
+import styled from "styled-components/native";
+
+export const Wrapper = styled.View`
+    position: relative;
+    display: flex;
+    width: 170px;
+    height: 200px;
+    margin: 19px;
+`;
+export const Category = styled.Text`
+    color: ${props => props.theme.colors.primary};
+    display: flex;
+    font-weight: 700;
+    font-size: 14px;
+`;
+export const Title = styled.Text`
+    color: ${props => props.theme.colors.black};
+    justify-content: center;
+    display: flex;
+    font-weight: 700;
+    height: 21px;
+    font-size: 16.42px;
+`;
+export const Price = styled.Text`
+    color: ${props => props.theme.colors.primary};
+    display: flex;
+    font-weight: 700;
+    font-size: 20px;
+`;
+export const Button = styled.TouchableOpacity`
+    box-sizing: border-box;
+    display: flex;
+    border-radius: 20px;
+    gap: 10px;
+    left: 59px;
+    bottom: 30px;
+`;
+export const ImageContainer = styled.View`
+    flex-direction: column;
+    align-items: center;
+    height: 145px;
+    width: 155px;
+`;
